741e7c73487dc0b92ef1a847e5bc7ff2681bfeb2
DevExpressMVVMFramework <alexander.egorov@devexpress.com>
2014-06-09 11:34:29 +0400

    Examples

---
+++ Examples/ExampleFileDialogServices/View/OpenFileDialogView.SL.xaml.cs
@@ -0,0 +1,9 @@
+using System.Windows.Controls;
+
+namespace Example.View {
+    public partial class OpenFileDialogView : UserControl {
+        public OpenFileDialogView() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Examples/ExampleFileDialogServices/View/SaveFileDialogView.SL.xaml.cs
@@ -0,0 +1,9 @@
+using System.Windows.Controls;
+
+namespace Example.View {
+    public partial class SaveFileDialogView : UserControl {
+        public SaveFileDialogView() {
+            InitializeComponent();
+        }
+    }
+}
