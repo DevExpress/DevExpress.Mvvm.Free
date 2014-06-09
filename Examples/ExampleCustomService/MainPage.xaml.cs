8a4f1752f3390a33dbb87573bbce228117950859
DevExpressMVVMFramework <alexander.egorov@devexpress.com>
2014-06-09 11:34:50 +0400

    Examples

---
+++ Examples/ExampleCustomService/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+using System.Windows.Controls;
+
+namespace Example {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+        }
+    }
+}
