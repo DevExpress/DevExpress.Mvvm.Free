8b3ae426886a6b8cea8968e5a9f66a08bdac278f
DevExpressMVVMFramework <alexander.egorov@devexpress.com>
2014-06-09 11:32:34 +0400

    Examples

---
+++ Examples/ExamplePOCO/View/LoginView.xaml.cs
@@ -0,0 +1,9 @@
+using System.Windows.Controls;
+
+namespace Example.View {
+    public partial class LoginView : UserControl {
+        public LoginView() {
+            InitializeComponent();
+        }
+    }
+}
