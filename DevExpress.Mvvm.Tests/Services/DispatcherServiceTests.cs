81da327e0e55138154ebcb3860da397a1ce0eb09
DevExpressMVVMFramework <alexander.egorov@devexpress.com>
2014-06-10 13:51:48 +0400

    DispatcherService

---
+++ DevExpress.Mvvm.Tests/Services/DispatcherServiceTests.cs
@@ -0,0 +1,60 @@
+﻿#if !SILVERLIGHT
+using NUnit.Framework;
+#else
+using Microsoft.Silverlight.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+#endif
+using System.Windows.Controls;
+using System.Threading.Tasks;
+using System;
+using System.Windows.Media;
+using System.Threading;
+using System.Windows.Data;
+
+namespace DevExpress.Mvvm.UI.Tests {
+    [TestFixture]
+    public class DispatcherServiceTests : BaseWpfFixture {
+        [Test, Asynchronous]
+        public void DispatcherServiceTest() {
+            TestVM vm = new TestVM();
+            UserControl control = new UserControl() { DataContext = vm };
+            DispatcherService service = new DispatcherService();
+            Interactivity.Interaction.GetBehaviors(control).Add(service);
+            Window.Content = control;
+            EnqueueShowWindow();
+            EnqueueCallback(() => {
+                Assert.IsFalse(vm.IsProgress);
+                vm.Calculate();
+                Assert.IsTrue(vm.IsProgress);
+            });
+            EnqueueDelay(50);
+            EnqueueWindowUpdateLayout();
+            EnqueueCallback(() => {
+                Assert.IsFalse(vm.IsProgress);
+                Assert.IsTrue(vm.IsCompleted);
+            });
+            EnqueueTestComplete();
+            
+        }
+        class TestVM : ViewModelBase {
+            public IDispatcherService DispatcherService { get { return GetService<IDispatcherService>(); } }
+            public bool IsProgress { get; private set; }
+            public bool IsCompleted { get; private set; }
+            public TestVM() {
+                IsCompleted = false;
+                IsProgress = false;
+            }
+            public void Calculate() {
+                IsProgress = true;
+                Task.Factory.StartNew(CalcCore).ContinueWith(x => {
+                    DispatcherService.BeginInvoke(new Action(() => IsProgress = false));
+                });
+            }
+            void CalcCore() {
+                DispatcherService.BeginInvoke(() => {
+                    IsCompleted = true;
+                });
+            }
+        }
+    }
+}
