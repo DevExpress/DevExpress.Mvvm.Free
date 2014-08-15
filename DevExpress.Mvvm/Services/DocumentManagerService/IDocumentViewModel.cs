a19b0b60c8a11b982db6dc4a9c36f27d0f0c7171
DevExpressMVVMFramework <alexander.egorov@devexpress.com>
2014-08-15 11:38:12 +0400

    14.1.6

--- DevExpress.Mvvm/Services/DocumentManagerService/IDocumentViewModel.cs
+++ DevExpress.Mvvm/Services/DocumentManagerService/IDocumentViewModel.cs
@@ -1,10 +1,62 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 
 namespace DevExpress.Mvvm {
     public interface IDocumentViewModel {
         bool Close();
         object Title { get; }
+    }
+    public interface IDocumentContent {
+        IDocumentOwner DocumentOwner { get; set; }
+        object Title { get; }
+        void OnClose(CancelEventArgs e);
+        void OnDestroy();
+    }
+    public interface IDocumentOwner {
+        void Close(IDocumentContent documentContent, bool force = true);
+    }
+}
+namespace DevExpress.Mvvm.Native {
+    public static class DocumentViewModelHelper {
+        public static void OnClose(object documentContentOrDocumentViewModel, CancelEventArgs e) {
+            IDocumentContent documentContent = documentContentOrDocumentViewModel as IDocumentContent;
+            if(documentContent != null) {
+                documentContent.OnClose(e);
+                return;
+            }
+            IDocumentViewModel documentViewModel = documentContentOrDocumentViewModel as IDocumentViewModel;
+            if(documentViewModel != null) {
+                e.Cancel = !documentViewModel.Close();
+                return;
+            }
+        }
+        public static void OnDestroy(object documentContentOrDocumentViewModel) {
+            IDocumentContent documentContent = documentContentOrDocumentViewModel as IDocumentContent;
+            if(documentContent != null)
+                documentContent.OnDestroy();
+        }
+        public static bool IsDocumentContentOrDocumentViewModel(object viewModel) {
+            return viewModel is IDocumentContent || viewModel is IDocumentViewModel;
+        }
+        public static bool TitlePropertyHasImplicitImplementation(object documentContentOrDocumentViewModel) {
+            IDocumentContent documentContent = documentContentOrDocumentViewModel as IDocumentContent;
+            if(documentContent != null)
+                return ExpressionHelper.PropertyHasImplicitImplementation(documentContent, i => i.Title);
+            IDocumentViewModel documentViewModel = documentContentOrDocumentViewModel as IDocumentViewModel;
+            if(documentViewModel != null)
+                return ExpressionHelper.PropertyHasImplicitImplementation(documentViewModel, i => i.Title);
+            throw new ArgumentException("", "documentContentOrDocumentViewModel");
+        }
+        public static object GetTitle(object documentContentOrDocumentViewModel) {
+            IDocumentContent documentContent = documentContentOrDocumentViewModel as IDocumentContent;
+            if(documentContent != null)
+                return documentContent.Title;
+            IDocumentViewModel documentViewModel = documentContentOrDocumentViewModel as IDocumentViewModel;
+            if(documentViewModel != null)
+                return documentViewModel.Title;
+            throw new ArgumentException("", "documentContentOrDocumentViewModel");
+        }
     }
 }
