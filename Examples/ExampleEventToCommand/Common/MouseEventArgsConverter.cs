ee22f55f9c16f11f74731c6061a622e6174b440b
AlexanderEgorov <alexander.egorov.e@gmail.com>
2014-09-26 12:08:22 +0400

    14.1.7

---
+++ Examples/ExampleEventToCommand/Common/MouseEventArgsConverter.cs
@@ -0,0 +1,14 @@
+﻿using DevExpress.Mvvm.UI;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Linq;
+
+namespace Example.Common {
+    public class ListBoxEventArgsConverter : EventArgsConverterBase<MouseEventArgs> {
+        protected override object Convert(object sender, MouseEventArgs args) {
+            var element = LayoutTreeHelper.GetVisualParents((DependencyObject)args.OriginalSource, (DependencyObject)sender).OfType<ListBoxItem>().FirstOrDefault();
+            return element != null ? element.DataContext : null;
+        }
+    }
+}
---
+++ Examples/ExampleEventToCommand/ViewModel/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using DevExpress.Mvvm;
+using DevExpress.Mvvm.DataAnnotations;
+using DevExpress.Mvvm.POCO;
+using System.Collections.ObjectModel;
+
+namespace Example.ViewModel {
+    [POCOViewModel]
+    public class MainViewModel {
+        public virtual ObservableCollection<Person> Persons { get; protected set; }
+        protected virtual IMessageBoxService MessageBoxService { get { return null; } }
+        public void Initialize() {
+            Persons = new ObservableCollection<Person>();
+            Persons.Add(ViewModelSource.Create(() => new Person() { FirstName = "John", LastName = "Smith" }));
+            Persons.Add(ViewModelSource.Create(() => new Person() { FirstName = "Alex", LastName = "Carter" }));
+        }
+        public void Edit(Person person) {
+            MessageBoxService.Show(string.Format("{0} {1}", person.FirstName, person.LastName));
+        }
+        public bool CanEdit(Person person) {
+            return person != null;
+        }
+    }
+    public class Person {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
